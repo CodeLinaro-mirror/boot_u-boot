b7e26407393a6594f1e5ef247046eef886db8b87
Ram Kumar D <quic_ramd@quicinc.com>
2023-07-03 20:33:29 +0530

    drivers: net: phy: add QTI8081 PHY driver support
    This patch adds QTI8081 PHY driver support and enables
    it in the ipq9574 platform.
    
    Change-Id: I60aadb5edb15f3e79a094f0f61026973db0a9d44
    Signed-off-by: Ram Kumar D <quic_ramd@quicinc.com>

--- drivers/net/phy/qti_8081.c
+++ drivers/net/phy/qti_8081.c
@@ -11,114 +11,103 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
 */
+
 #include <common.h>
-#include <net.h>
-#include <asm-generic/errno.h>
+#include <command.h>
 #include <asm/io.h>
-#include <malloc.h>
 #include <phy.h>
-#include "ipq_qca8081.h"
-#include "ipq_phy.h"
+#include <miiphy.h>
 
-extern int ipq_mdio_read(int mii_id,
-		int regnum, ushort *data);
-extern int ipq_mdio_write(int mii_id,
-		int regnum, u16 data);
+#include "qti.h"
 
-u16 qca8081_phy_reg_read(u32 dev_id, u32 phy_id, u32 reg_id)
-{
-	return ipq_mdio_read(phy_id, reg_id, NULL);
-}
+#define QTI_8081_PHY_V1				0x004DD100
+#define QTI_8081_PHY_V1_1			0x004DD101
 
-u16 qca8081_phy_reg_write(u32 dev_id, u32 phy_id, u32 reg_id, u16 value)
-{
-	return ipq_mdio_write(phy_id, reg_id, value);
-}
+#define QTI_8081_PHY_SPEC_STATUS                 17
+#define QTI_8081_STATUS_LINK_PASS                0x0400
+#define QTI_8081_STATUS_FULL_DUPLEX              0x2000
+#define QTI_8081_STATUS_SPEED_MASK               0x380
+#define QTI_8081_STATUS_SPEED_2500MBS            0x200
+#define QTI_8081_STATUS_SPEED_1000MBS            0x100
+#define QTI_8081_STATUS_SPEED_100MBS             0x80
+#define QTI_8081_STATUS_SPEED_10MBS              0x0000
 
-u8 qca8081_phy_get_link_status(u32 dev_id, u32 phy_id)
-{
-	u16 phy_data;
-	phy_data = qca8081_phy_reg_read(dev_id,
-			phy_id, QCA8081_PHY_SPEC_STATUS);
-	if (phy_data & QCA8081_STATUS_LINK_PASS)
-		return 0;
+#define QTI_8081_PHY_MMD3_NUM                    3
 
-	return 1;
-}
+#define QTI_8081_PHY_MMD3_ADDR_CLD_CTRL7         0x8007
+#define QTI_8081_PHY_8023AZ_AFE_CTRL_MASK        0x01f0
+#define QTI_8081_PHY_8023AZ_AFE_EN               0x0090
 
-u32 qca8081_phy_get_duplex(u32 dev_id, u32 phy_id, fal_port_duplex_t *duplex)
+#define QTI_8081_PHY_MMD3_AZ_TRAINING_CTRL       0x8008
+#define QTI_8081_PHY_MMD3_AZ_TRAINING_VAL        0x1c32
+
+
+static int qti_8081_config(struct phy_device *phydev)
 {
 	u16 phy_data;
 
-	phy_data = qca8081_phy_reg_read(dev_id, phy_id,
-				QCA8081_PHY_SPEC_STATUS);
+	/* Enable vga when init napa to fix 8023az issue */
+	phy_data = phy_read_mmd(phydev, QTI_8081_PHY_MMD3_NUM,
+			QTI_8081_PHY_MMD3_ADDR_CLD_CTRL7);
+	phy_data &= (~QTI_8081_PHY_8023AZ_AFE_CTRL_MASK);
+	phy_data |= QTI_8081_PHY_8023AZ_AFE_EN;
+	phy_write_mmd(phydev, QTI_8081_PHY_MMD3_NUM,
+			QTI_8081_PHY_MMD3_ADDR_CLD_CTRL7, phy_data);
 
-	/*
-	 * Read duplex
-	 */
-	if (phy_data & QCA8081_STATUS_FULL_DUPLEX)
-		*duplex = FAL_FULL_DUPLEX;
-	else
-		*duplex = FAL_HALF_DUPLEX;
-
+	/* Special configuration for AZ under 1G speed mode */
+	phy_write_mmd(phydev, QTI_8081_PHY_MMD3_NUM,
+			QTI_8081_PHY_MMD3_AZ_TRAINING_CTRL,
+			QTI_8081_PHY_MMD3_AZ_TRAINING_VAL);
 	return 0;
 }
 
-u32 qca8081_phy_get_speed(u32 dev_id, u32 phy_id, fal_port_speed_t *speed)
+static int qti_8081_startup(struct phy_device *phydev)
 {
 	u16 phy_data;
 
-	phy_data = qca8081_phy_reg_read(dev_id,
-			phy_id, QCA8081_PHY_SPEC_STATUS);
+	phy_data = phy_read(phydev, MDIO_DEVAD_NONE, QTI_8081_PHY_SPEC_STATUS);
+	if (phy_data & QTI_8081_STATUS_LINK_PASS)
+		phydev->link = 1;
+	else
+		phydev->link = 0;
 
-	switch (phy_data & QCA8081_STATUS_SPEED_MASK) {
-	case QCA8081_STATUS_SPEED_2500MBS:
-		*speed = FAL_SPEED_2500;
+	if (phy_data & QTI_8081_STATUS_FULL_DUPLEX)
+		phydev->duplex = DUPLEX_FULL;
+	else
+		phydev->duplex = DUPLEX_HALF;
+
+	switch (phy_data & QTI_8081_STATUS_SPEED_MASK) {
+	case QTI_8081_STATUS_SPEED_2500MBS:
+		phydev->speed = SPEED_2500;
 		break;
-	case QCA8081_STATUS_SPEED_1000MBS:
-		*speed = FAL_SPEED_1000;
+	case QTI_8081_STATUS_SPEED_1000MBS:
+		phydev->speed = SPEED_1000;
 		break;
-	case QCA8081_STATUS_SPEED_100MBS:
-		*speed = FAL_SPEED_100;
+	case QTI_8081_STATUS_SPEED_100MBS:
+		phydev->speed = SPEED_100;
 		break;
-	case QCA8081_STATUS_SPEED_10MBS:
-		*speed = FAL_SPEED_10;
+	case QTI_8081_STATUS_SPEED_10MBS:
+		phydev->speed = SPEED_10;
 		break;
 	default:
 		return -EINVAL;
 	}
+
 	return 0;
 }
 
-int ipq_qca8081_phy_init(struct phy_ops **ops, u32 phy_id)
+static struct phy_driver qti_8081_driver = {
+	.name = "QTI 8081 PHY Driver",
+	.uid = QTI_8081_PHY_V1,
+	.mask = 0xfffffff0,
+	.features = PHY_GBIT_FEATURES,
+	.config = &qti_8081_config,
+	.startup = &qti_8081_startup,
+	.shutdown = &genphy_shutdown,
+};
+
+int phy_8081_init(void)
 {
-	u16 phy_data;
-	struct phy_ops *qca8081_ops;
-
-	qca8081_ops = (struct phy_ops *)malloc(sizeof(struct phy_ops));
-	if (!qca8081_ops)
-		return -ENOMEM;
-	qca8081_ops->phy_get_link_status = qca8081_phy_get_link_status;
-	qca8081_ops->phy_get_speed = qca8081_phy_get_speed;
-	qca8081_ops->phy_get_duplex = qca8081_phy_get_duplex;
-	*ops = qca8081_ops;
-
-	phy_data = qca8081_phy_reg_read(0x0, phy_id, QCA8081_PHY_ID1);
-	printf ("PHY ID1: 0x%x\n", phy_data);
-	phy_data = qca8081_phy_reg_read(0x0, phy_id, QCA8081_PHY_ID2);
-	printf ("PHY ID2: 0x%x\n", phy_data);
-
-	/*enable vga when init napa to fix 8023az issue*/
-	phy_data = qca8081_phy_reg_read(0x0, phy_id, QCA808X_8023AZ_ENABLE_VGA);
-	phy_data &= (~QCA808X_PHY_8023AZ_AFE_CTRL_MASK);
-	phy_data |= QCA808X_PHY_8023AZ_AFE_EN;
-	phy_data = qca8081_phy_reg_write(0x0, phy_id, QCA808X_8023AZ_ENABLE_VGA, phy_data);
-	if (phy_data != 0)
-		return phy_data;
-
-	/*special configuration for AZ under 1G speed mode*/
-	phy_data = QCA808X_PHY_MMD3_AZ_TRAINING_VAL;
-	phy_data = qca8081_phy_reg_write(0x0, phy_id, QCA808X_AZ_CONFIG_UNDER_1G_SPEED,
-					phy_data);
-	return phy_data;
+	phy_register(&qti_8081_driver);
+	return 0;
 }
