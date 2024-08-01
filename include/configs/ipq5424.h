c2d9184b3eacb60eef49203af7198cb5c2bb1c7d
Ram Kumar D <quic_ramd@quicinc.com>
2024-08-01 13:08:43 +0530

    configs: ipq5424: remove CFG_EMULATION
    Change-Id: I4551f7b90c5adddb699ab36fd34aca136bd8775d
    Signed-off-by: Ram Kumar D <quic_ramd@quicinc.com>

--- include/configs/ipq5424.h
+++ include/configs/ipq5424.h
@@ -53,7 +53,6 @@ extern uint32_t g_load_addr;
 #define CFG_SYS_BAUDRATE_TABLE			{ 115200, 230400,	\
 							460800, 921600 }
 
-#define CFG_EMULATION
 
 /* override the counter frequency incase of emulation platform */
 #ifdef CFG_EMULATION
