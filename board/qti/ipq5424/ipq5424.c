7d440f3a14d12846a590df813eeda52ab00bec3f
Vijay Balaji <vijbal@qti.qualcomm.com>
2025-09-24 15:37:06 +0530

    driver: nand: Add new QSPI-NAND device in list
    Here we are updating QPSI nand device with chip size
    256K to support RDP485-C4. Based on RDP485-C3 variant.
    
    Change-Id: If992dc145b6dffd11b2a3779b15efaf6e04d0511
    Signed-off-by: Vijay Balaji <vijbal@qti.qualcomm.com>
    (cherry picked from commit 6f4eda74ac4656c1d34a37ed7d3df98cdf6320da)

--- board/qti/ipq5424/ipq5424.c
+++ board/qti/ipq5424/ipq5424.c
@@ -69,6 +69,7 @@ struct machid_dts_map machid_dts[] = {
 	{ MACH_TYPE_IPQ5424_RDP485, "ipq5424-rdp485"},
 	{ MACH_TYPE_IPQ5424_RDP485_C2, "ipq5424-rdp485-c2"},
 	{ MACH_TYPE_IPQ5424_RDP485_C3, "ipq5424-rdp485"},
+	{ MACH_TYPE_IPQ5424_RDP485_C4, "ipq5424-rdp485"},
 	{ MACH_TYPE_IPQ5424_RDP487, "ipq5424-rdp487"},
 	{ MACH_TYPE_IPQ5424_RDP487_C2, "ipq5424-rdp487"},
 	{ MACH_TYPE_IPQ5424_RDP487_C3, "ipq5424-rdp487-c3"},
@@ -491,6 +492,9 @@ void ipq_uboot_fdt_fixup(uint32_t machid)
 			break;
 		case MACH_TYPE_IPQ5424_RDP485_C3:
 			config_list[config_nos++] = "config-rdp485-c3";
+			break;
+		case MACH_TYPE_IPQ5424_RDP485_C4:
+			config_list[config_nos++] = "config-rdp485-c4";
 			break;
 		case MACH_TYPE_IPQ5424_RDP487_C2:
 			config_list[config_nos++] = "config-rdp487-c2";
